e765f8f7973def250e98dc8c71097b937dc8acfc
Исаков Айрат Амирович <aaisakov@sberbank.ru>
2024-02-22 11:43:30 +0300

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { standard } from "@salutejs/plasma-typo";
+import { SSRProvider } from "@salutejs/sdds-serv";
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+// import { sdds_serv__light } from "@salutejs/sdds-serv/themes";
+
+const TypoStyle = createGlobalStyle(standard);
+// const ThemeStyle = createGlobalStyle(sdds_serv__light);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SSRProvider>
+        <TypoStyle />
+        <Component {...pageProps} />
+      </SSRProvider>
+    </>
+  );
 }
